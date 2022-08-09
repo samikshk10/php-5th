70309387e552f319fa1f2c01e7984149d1177a54
samik shk <samik.shakya8848@gmail.com>
2022-08-09 18:28:34 +0545

    initial

---
+++ assignment/first.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first php webpage</title>
+
+
+</head>
+<body>
+    <h2>My first php web page</h2>
+ 
+            <?php  
+               define('FULLNAME','Samik Shakya');               
+    echo "Hello World!!!!!! <br> I am ",FULLNAME;
+    ?>
+ 
+</body>
+</html>
