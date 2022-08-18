994bfe66c16a1d44f7274f4d46a5e742eadc03cf
samik shk <samik.shakya8848@gmail.com>
2022-08-18 21:06:46 +0545

    modifed

--- assignment 1/q1.php
+++ assignment 1/q1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Question 1</title>
 </head>
 <body>
 <?php
@@ -13,7 +13,7 @@ function evaluate($name,$attendance,$hygiene,$rating)
 {
   if($attendance<80 || $hygiene<50 || $rating<2)
   {
-     echo $name ,"<br>You are fired from Rojan kirana pasal ";
+     echo $name ,".....You are fired from Rojan kirana pasal ";
   }
 
   else
@@ -21,8 +21,8 @@ function evaluate($name,$attendance,$hygiene,$rating)
       echo "You are not fired for now!!!";
   }
 }
-$ename="depen";
-$eattendance=30;
+$ename="kusal karki";
+$eattendance=20;
 $ehygiene=70;
 $erating=3;
 evaluate($ename,$eattendance,$ehygiene,$erating)
--- assignment 1/q2.php
+++ assignment 1/q2.php
@@ -4,22 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Q2</title>
+    <title>Question 2</title>
 </head>
 <body>
     <?php 
         function evaluate($name, $attendence, $hygiene, $rating){
             if(($attendence < 80 && $hygiene < 50) || ($hygiene < 50 && $rating < 2)){
-                echo $name, "You are fired from  Rojan Kirana Pasal";
-            } else {
+                echo $name, ".....You are fired from  Rojan Kirana Pasal";
+            }
+             else {
                 echo "Not fired for now";
             }
         }
-        $ename="kusal karki";
-$eattendance=20;
-$ehygiene=70;
-$erating=3;
-evaluate($ename,$eattendance,$ehygiene,$erating)
+    
+evaluate("depen",60,50,2)
     ?>
 </body>
 </html>
